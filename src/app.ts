e406a739dd6b08d3469eb391fbc0d1969f49472d
Alex Prismont <aprishmont@gmail.com>
2021-03-21 14:41:58 +0200

    [FEATURE] Remove all Notes which are related to the deleted Board

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,22 @@ import express, { Request } from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { getUser } from './utils/context';
 import schema from './modules';
+import cors from 'cors';
 
 const server: ApolloServer = new ApolloServer({
   schema,
   context: async (ctx: {req: Request}) => ({
     user: await getUser(ctx.req),
   }),
+  cacheControl: {
+    defaultMaxAge: 0,
+  },
 });
 
 const app: express.Application = express();
 
 server.applyMiddleware({ app });
+
+app.use(cors());
 
 export const App = app;
